368500c93fd9b0611d0f5ac0491ce49d696ae9dd
ToddBodnar <tjb5215@psu.edu>
2016-03-09 22:43:23 -0800

    garbage collector layout

---
+++ SimpleHive/src/main/java/com/toddbodnar/simpleHive/helpers/garbage_collector.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toddbodnar.simpleHive.helpers;
+
+import com.toddbodnar.simpleHive.IO.file;
+import java.util.HashSet;
+
+/**
+ *
+ * @author toddbodnar
+ */
+public class garbage_collector {
+    private static HashSet<file> auto_files = new HashSet<file>(); //set of files algorithmically created (which may not necessarily be kept over time
+    
+    public static void noteCreated(file theFile)
+    {
+        auto_files.add(theFile);
+    }
+    
+    public static void persist(file theFile)
+    {
+        auto_files.remove(theFile);
+    }
+    
+    public static int collect()
+    {
+        //files not removed by the gc call
+        HashSet<file> new_auto_files = new HashSet<file>();
+        
+        for(file f:auto_files)
+        {
+            //TODO: test if auto_files have been added to metastore
+            
+            //TODO: remove file if not
+        }
+    }
+    
+    public static String stats()
+    {
+        return "TODO: gc stats!";
+    }
+}
